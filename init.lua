ce46cae83be0e969b447d9b2c303cc636f5895e8
Logan MacDougall <lg463300@dal.ca>
2024-09-23 12:18:03 -0300

    Added spellchecking to config

--- init.lua
+++ init.lua
@@ -243,6 +243,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Add spellcheck to my files
+vim.opt.apelllang = 'en_us'
+vim.opt.spell = true
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
